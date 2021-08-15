7beafcc29c75ec354f8c9fd5c1b7f8f8a7b3f8de
Phals3 <phalse3@gmail.com>
2021-08-15 23:20:24 +0200

    init

---
+++ snippets/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetails.as_view())
+]
